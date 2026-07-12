7f7002cf140723fecc2b209c1168123c70158ca2
dentiny <dentinyhao@gmail.com>
2026-07-11 19:07:47 -0700

    Register opendalfs (#27)

--- src/duckdb_opendalfs_extension.cpp
+++ src/duckdb_opendalfs_extension.cpp
@@ -3,43 +3,23 @@
 #include "duckdb_opendalfs_extension.hpp"
 #include "duckdb.hpp"
 #include "duckdb/common/exception.hpp"
+#include "duckdb/common/opener_file_system.hpp"
 #include "duckdb/function/scalar_function.hpp"
+#include "opendal_file_system.hpp"
 #include <duckdb/parser/parsed_data/create_scalar_function_info.hpp>
 
 #include <opendal.hpp>
 
 namespace duckdb {
 
-inline void DuckDBOpenDALFSScalarFun(DataChunk &args, ExpressionState &state, Vector &result) {
-	auto &name_vector = args.data[0];
-	UnaryExecutor::Execute<string_t, string_t>(name_vector, result, args.size(), [&](string_t name) {
-		return StringVector::AddString(result, "...........🦆 " + name.GetString());
-	});
+namespace {
+void LoadInternal(ExtensionLoader &loader) {
+	auto &instance = loader.GetDatabaseInstance();
+	auto &opener_filesystem = instance.GetFileSystem().Cast<OpenerFileSystem>();
+	auto &vfs = opener_filesystem.GetFileSystem();
+	vfs.RegisterSubSystem(make_uniq<OpenDALFileSystem>());
 }
-
-inline void DuckDBOpenDALFSOpenDALAvailableScalarFun(DataChunk &args, ExpressionState &state, Vector &result) {
-	auto &name_vector = args.data[0];
-	UnaryExecutor::Execute<string_t, string_t>(name_vector, result, args.size(), [&](string_t name) {
-		opendal::Operator op("memory");
-		return StringVector::AddString(result, "DuckDBOpenDALFS " + name.GetString() + ", OpenDAL memory service is " +
-		                                           (op.Available() ? "available" : "unavailable"));
-	});
-}
-
-static void LoadInternal(ExtensionLoader &loader) {
-	// Register a scalar function
-	auto duckdb_opendalfs_scalar_function =
-	    ScalarFunction("duckdb_opendalfs", {LogicalType::VARCHAR}, LogicalType::VARCHAR, DuckDBOpenDALFSScalarFun);
-
-	loader.RegisterFunction(duckdb_opendalfs_scalar_function);
-
-	// This small probe also ensures that OpenDAL's C++ and Rust objects are
-	// retained and linked into both extension variants.
-	auto duckdb_opendalfs_opendal_available_scalar_function =
-	    ScalarFunction("duckdb_opendalfs_opendal_available", {LogicalType::VARCHAR}, LogicalType::VARCHAR,
-	                   DuckDBOpenDALFSOpenDALAvailableScalarFun);
-	loader.RegisterFunction(duckdb_opendalfs_opendal_available_scalar_function);
-}
+} // namespace
 
 void DuckdbOpendalfsExtension::Load(ExtensionLoader &loader) {
 	LoadInternal(loader);
